6ac6cda6fe5ae71f12426bd721dd8c60cde922f4
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-02-27 14:47:05 +0200

    feat/styling weather component

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -22,7 +22,7 @@ function Weather() {
   const [data, setData] = useState<WeatherData>({});
   const [location, setLocation] = useState<string>('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -50,14 +50,14 @@ function Weather() {
           <div className="location">
             <p>{data.name}</p>
           </div>
-          <div className="temp">{data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}</div>
+          <div className="temp">{data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}</div>
           <div className="description">{data.weather ? <p>{data.weather[0].main}</p> : null}</div>
         </div>
 
         {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°F</p> : null}
+              {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
